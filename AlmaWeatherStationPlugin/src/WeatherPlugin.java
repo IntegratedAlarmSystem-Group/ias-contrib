122f31940158102756bd445349495726dcdd3471
Jazmine <jamaldonadof@ug.uchile.cl>
2018-04-02 12:12:10 -0300

    Fix code formatting

--- AlmaWeatherStationPlugin/src/WeatherPlugin.java
+++ AlmaWeatherStationPlugin/src/WeatherPlugin.java
@@ -24,191 +24,194 @@ import java.util.concurrent.*;
  */
 public class WeatherPlugin extends Plugin {
 
-  /**
-   * runs the plugin.
-   *
-   * @param args .
-   */
-  public static void main(String[] args) {
+	/**
+	 * runs the plugin.
+	 *
+	 * @param args
+	 *            .
+	 */
+	public static void main(String[] args) {
 
-    logger.info("Started...");
-    PluginConfig config = null;
-    try {
-      File configFile = new File(configPath);
-      PluginConfigFileReader jsonFileReader = new PluginConfigFileReader(configFile);
-      Future<PluginConfig> futurePluginConfig = jsonFileReader.getPluginConfig();
-      config = futurePluginConfig.get(1, TimeUnit.MINUTES);
-    } catch (FileNotFoundException e) {
-      logger.error("Excetion opening config file", e);
-      e.printStackTrace();
-      System.exit(-1);
-    } catch (PluginConfigException pce) {
-      logger.error("Excetion reading configuratiopn", pce);
-      pce.printStackTrace();
-      System.exit(-1);
-    } catch (InterruptedException ie) {
-      logger.error("Interrupted", ie);
-      ie.printStackTrace();
-      System.exit(-1);
-    } catch (TimeoutException te) {
-      logger.error("Timeout reading configuration", te);
-      te.printStackTrace();
-      System.exit(-1);
-    } catch (ExecutionException ee) {
-      ee.printStackTrace();
-      logger.error("Execution error", ee);
-      System.exit(-1);
-    }
-    logger.info("Configuration successfully read");
+		logger.info("Started...");
+		PluginConfig config = null;
+		try {
+			File configFile = new File(configPath);
+			PluginConfigFileReader jsonFileReader = new PluginConfigFileReader(configFile);
+			Future<PluginConfig> futurePluginConfig = jsonFileReader.getPluginConfig();
+			config = futurePluginConfig.get(1, TimeUnit.MINUTES);
+		} catch (FileNotFoundException e) {
+			logger.error("Exception opening config file", e);
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (PluginConfigException pce) {
+			logger.error("Exception reading configuration", pce);
+			pce.printStackTrace();
+			System.exit(-1);
+		} catch (InterruptedException ie) {
+			logger.error("Interrupted", ie);
+			ie.printStackTrace();
+			System.exit(-1);
+		} catch (TimeoutException te) {
+			logger.error("Timeout reading configuration", te);
+			te.printStackTrace();
+			System.exit(-1);
+		} catch (ExecutionException ee) {
+			ee.printStackTrace();
+			logger.error("Execution error", ee);
+			System.exit(-1);
+		}
+		logger.info("Configuration successfully read");
 
 		String sinkServer = config.getSinkServer();
-    int sinkPort = config.getSinkPort();
+		int sinkPort = config.getSinkPort();
 
-    logger.info("args length = "+args.length);
-    if (args.length > 0) {
-      sinkServer = args[0];
-      if (args.length > 1) {
-        try {
-          sinkPort = Integer.parseInt(args[1]);
-        } catch (NumberFormatException e) {
-          System.err.println("Sink port" + args[1] + " must be an integer.");
-          System.exit(1);
-        }
-      }
-    }
-    logger.info("Kafka sink server: "+sinkServer+":"+sinkPort);
-    config.setSinkServer(sinkServer);
-    config.setSinkPort(sinkPort);
+		logger.info("args length = " + args.length);
+		if (args.length > 0) {
+			sinkServer = args[0];
+			if (args.length > 1) {
+				try {
+					sinkPort = Integer.parseInt(args[1]);
+				} catch (NumberFormatException e) {
+					System.err.println("Sink port" + args[1] + " must be an integer.");
+					System.exit(1);
+				}
+			}
+		}
+		logger.info("Kafka sink server: " + sinkServer + ":" + sinkPort);
+		config.setSinkServer(sinkServer);
+		config.setSinkPort(sinkPort);
 
-    KafkaPublisher kafkaPublisher = new KafkaPublisher(config.getId(), config.getMonitoredSystemId(),
-        config.getSinkServer(), config.getSinkPort(), Plugin.getScheduledExecutorService());
+		KafkaPublisher kafkaPublisher = new KafkaPublisher(config.getId(), config.getMonitoredSystemId(),
+				config.getSinkServer(), config.getSinkPort(), Plugin.getScheduledExecutorService());
 
-    WeatherPlugin plugin = new WeatherPlugin(config, kafkaPublisher);
+		WeatherPlugin plugin = new WeatherPlugin(config, kafkaPublisher);
 
-    try {
-      plugin.start();
-    } catch (PublisherException pe) {
-      logger.error("The plugin failed to start", pe);
-      System.exit(-3);
-    }
+		try {
+			plugin.start();
+		} catch (PublisherException pe) {
+			logger.error("The plugin failed to start", pe);
+			System.exit(-3);
+		}
 
-    // Connect to the weather station.
-    plugin.initialize();
-    plugin.setPluginOperationalMode(OperationalMode.OPERATIONAL);
+		// Connect to the weather station.
+		plugin.initialize();
+		plugin.setPluginOperationalMode(OperationalMode.OPERATIONAL);
 
-    // Start getting data from the weather station
-    // This method exits when the user presses CTRL+C
-    // and the shutdown hook disconnects from the weather station.
-    plugin.startLoop();
+		// Start getting data from the weather station
+		// This method exits when the user presses CTRL+C
+		// and the shutdown hook disconnects from the weather station.
+		plugin.startLoop();
 
-    logger.info("Configuration done.");
-  }
+		logger.info("Configuration done.");
+	}
 
-  /**
-   * The logger.
-   */
-  private static final Logger logger = LoggerFactory.getLogger(WeatherPlugin.class);
+	/**
+	 * The logger.
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(WeatherPlugin.class);
 
-  /**
-   * the loop to keep the plugin running.
-   */
-  private ScheduledFuture<?> loopFuture;
+	/**
+	 * the loop to keep the plugin running.
+	 */
+	private ScheduledFuture<?> loopFuture;
 
-  /**
-   * the weather station to retrieve the data.
-   */
-  private WeatherStation weatherStation;
+	/**
+	 * the weather station to retrieve the data.
+	 */
+	private WeatherStation weatherStation;
 
-  /**
-   * The path to the config file for the plugin.
-   */
-  private static final String configPath = "config.json";
-  
-  private static Collection<Value> values;
-  
-  private static Map<String,String> monitorPoints = new HashMap<String, String>();
+	/**
+	 * The path to the config file for the plugin.
+	 */
+	private static final String configPath = "config.json";
 
-  /**
-   * Constructor
-   *
-   * @param config The configuration of the plugin.
-   * @param sender The sender.
-   */
-  private WeatherPlugin(PluginConfig config, MonitorPointSender sender) {
-    super(config, sender);
-    values = config.getValuesAsCollection();
-    Property[] props = config.getProperties();
-    for (Property prop : props) {
-    	monitorPoints.put(prop.getKey(), prop.getValue());
-    }
-  }
+	private static Collection<Value> values;
 
-  /**
-   * Connect to the Weather Station and add the shutdown hook.
-   */
-  private void initialize() {
-    // refreshes every 1000 milliseconds
-    weatherStation = new WeatherStation(1, 11, 1000);
+	private static Map<String, String> monitorPoints = new HashMap<String, String>();
 
-    // Adds the shutdown hook
-    Runtime.getRuntime().addShutdownHook(new Thread(this::cleanUp, "Release weather station shutdown hook"));
-  }
+	/**
+	 * Constructor
+	 *
+	 * @param config
+	 *            The configuration of the plugin.
+	 * @param sender
+	 *            The sender.
+	 */
+	private WeatherPlugin(PluginConfig config, MonitorPointSender sender) {
+		super(config, sender);
+		values = config.getValuesAsCollection();
+		Property[] props = config.getProperties();
+		for (Property prop : props) {
+			monitorPoints.put(prop.getKey(), prop.getValue());
+		}
+	}
 
-  /**
-   * Terminate the thread that publishes the data and disconnects from the weather station.
-   */
-  private void cleanUp() {
-    if (loopFuture != null) {
-      loopFuture.cancel(false);
-    }
-    weatherStation.release();
-  }
+	/**
+	 * Connect to the Weather Station and add the shutdown hook.
+	 */
+	private void initialize() {
+		// refreshes every 1000 milliseconds
+		weatherStation = new WeatherStation(1, 11, 1000);
 
-  /**
-   * Override method to catch the exception and log a message
-   * <p>
-   * In the example we do not take any special action if the Plugin returns an
-   * error when submitting a new value.
-   */
-  @Override
-  public void updateMonitorPointValue(String mPointID, Object value) {
-    try {
-      super.updateMonitorPointValue(mPointID, value);
-    } catch (PluginException pe) {
-      logger.error("Error sending {} monitor point to the core of the IAS", mPointID);
-    }
-  }
+		// Adds the shutdown hook
+		Runtime.getRuntime().addShutdownHook(new Thread(this::cleanUp, "Release weather station shutdown hook"));
+	}
 
-  /**
-   * The loop to get monitor values from the weather station
-   * and update the values sent to the core of the IAS.
-   */
-  private void startLoop() {
-    // send data every second.
-    loopFuture = getScheduledExecutorService().scheduleAtFixedRate(
-        () -> {
-          logger.debug("Updating monitor point values from the weather station");
-          
-          values.forEach(value -> {
-              try {
-            	String [] parts = monitorPoints.get(value.getId()).split("-");
-            	double v = weatherStation.getValue(Integer.parseInt(parts[0]), parts[1]);
+	/**
+	 * Terminate the thread that publishes the data and disconnects from the weather
+	 * station.
+	 */
+	private void cleanUp() {
+		if (loopFuture != null) {
+			loopFuture.cancel(false);
+		}
+		weatherStation.release();
+	}
 
-                updateMonitorPointValue(value.getId(), v);
+	/**
+	 * Override method to catch the exception and log a message
+	 * <p>
+	 * In the example we do not take any special action if the Plugin returns an
+	 * error when submitting a new value.
+	 */
+	@Override
+	public void updateMonitorPointValue(String mPointID, Object value) {
+		try {
+			super.updateMonitorPointValue(mPointID, value);
+		} catch (PluginException pe) {
+			logger.error("Error sending {} monitor point to the core of the IAS", mPointID);
+		}
+	}
 
-              } catch (Exception e) {
-                logger.error(e.getMessage());
-              }
-          });
-          
-          logger.debug("Monitor point values updated");
-        }, 0, 1, TimeUnit.SECONDS);
-    try {
-      loopFuture.get();
-    } catch (ExecutionException ee) {
-      logger.error("Execution exception getting values from the weather station", ee);
-    } catch (Exception e) {
-      logger.warn("Loop to get monitor point values from the weather station terminated");
-    }
-  }
+	/**
+	 * The loop to get monitor values from the weather station and update the values
+	 * sent to the core of the IAS.
+	 */
+	private void startLoop() {
+		// send data every second.
+		loopFuture = getScheduledExecutorService().scheduleAtFixedRate(() -> {
+			logger.debug("Updating monitor point values from the weather station");
+
+			values.forEach(value -> {
+				try {
+					String[] parts = monitorPoints.get(value.getId()).split("-");
+					double v = weatherStation.getValue(Integer.parseInt(parts[0]), parts[1]);
+
+					updateMonitorPointValue(value.getId(), v);
+
+				} catch (Exception e) {
+					logger.error(e.getMessage());
+				}
+			});
+
+			logger.debug("Monitor point values updated");
+		}, 0, 1, TimeUnit.SECONDS);
+		try {
+			loopFuture.get();
+		} catch (ExecutionException ee) {
+			logger.error("Execution exception getting values from the weather station", ee);
+		} catch (Exception e) {
+			logger.warn("Loop to get monitor point values from the weather station terminated");
+		}
+	}
 }
--- AlmaWeatherStationPlugin/src/WeatherSensor.java
+++ AlmaWeatherStationPlugin/src/WeatherSensor.java
@@ -13,202 +13,208 @@ import java.util.HashMap;
 import java.util.Objects;
 
 /**
- * A class that parses and saves weather data from http://weather.aiv.alma.cl/ws_weather.php,
- * to work it must be in a network from ALMA, ESO, NAOJ or NRAO.
+ * A class that parses and saves weather data from
+ * http://weather.aiv.alma.cl/ws_weather.php, to work it must be in a network
+ * from ALMA, ESO, NAOJ or NRAO.
  * <p>
  * The values can be updated using updateValues() or run() (for scheduling).
  */
 public class WeatherSensor implements Runnable {
 
-  // sensor testing
-  public static void main(String[] args) {
-    WeatherSensor sensor = new WeatherSensor(2, 2000);
-    sensor.updateValues();
-    System.out.println(sensor);
-  }
+	// sensor testing
+	public static void main(String[] args) {
+		WeatherSensor sensor = new WeatherSensor(2, 2000);
+		sensor.updateValues();
+		System.out.println(sensor);
+	}
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("id: ");
-    sb.append(id);
-    sb.append(System.lineSeparator());
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("id: ");
+		sb.append(id);
+		sb.append(System.lineSeparator());
 
-    for (String key : values.keySet()) {
-      sb.append("  ");
-      sb.append(key);
-      sb.append(": ");
-      sb.append(values.get(key));
-      sb.append(System.lineSeparator());
-    }
+		for (String key : values.keySet()) {
+			sb.append("  ");
+			sb.append(key);
+			sb.append(": ");
+			sb.append(values.get(key));
+			sb.append(System.lineSeparator());
+		}
 
-    return sb.toString().trim();
-  }
+		return sb.toString().trim();
+	}
 
-  /**
-   * The logger.
-   */
-  private static final Logger logger = LoggerFactory.getLogger(WeatherPlugin.class);
+	/**
+	 * The logger.
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(WeatherPlugin.class);
 
-  /**
-   * sensor id the will be sent in the soap request.
-   */
-  private int id;
+	/**
+	 * sensor id the will be sent in the soap request.
+	 */
+	private int id;
 
-  /**
-   * DOM parser.
-   */
-  private DocumentBuilder dBuilder;
+	/**
+	 * DOM parser.
+	 */
+	private DocumentBuilder dBuilder;
 
-  /**
-   * soap client.
-   */
-  private SOAPRequest soap;
+	/**
+	 * soap client.
+	 */
+	private SOAPRequest soap;
 
-  /**
-   * sensor values {temperature, pressure, dewpoint, humidity, wind speed, wind
-   * direction}.
-   */
-  private HashMap<String, Double> values = new HashMap<>();
+	/**
+	 * sensor values {temperature, pressure, dewpoint, humidity, wind speed, wind
+	 * direction}.
+	 */
+	private HashMap<String, Double> values = new HashMap<>();
 
-  /**
-   * The timestamp from the last time the values were updated.
-   */
-  private long lastUpdated = 0;
+	/**
+	 * The timestamp from the last time the values were updated.
+	 */
+	private long lastUpdated = 0;
 
-  /**
-   * time to live of the values in this plugin, in milliseconds.
-   */
-  private long ttl;
+	/**
+	 * time to live of the values in this plugin, in milliseconds.
+	 */
+	private long ttl;
 
-  /**
-   * creates a sensor with the given id, the id is the value given in the soap
-   * request to access the data.
-   *
-   * @param id of the sensor to request.
-   */
-  WeatherSensor(int id, int timeToLive) {
-    // soap requests
-    String url = "http://weather.aiv.alma.cl/ws_weather.php";
-    String action = "getCurrentWeatherData";
-    String idName = "id";
-    soap = new SOAPRequest(url, url, action, idName);
+	/**
+	 * creates a sensor with the given id, the id is the value given in the soap
+	 * request to access the data.
+	 *
+	 * @param id
+	 *            of the sensor to request.
+	 */
+	WeatherSensor(int id, int timeToLive) {
+		// soap requests
+		String url = "http://weather.aiv.alma.cl/ws_weather.php";
+		String action = "getCurrentWeatherData";
+		String idName = "id";
+		soap = new SOAPRequest(url, url, action, idName);
 
-    createDOMBuilder();
+		createDOMBuilder();
 
-    // sensor id
-    this.id = id;
-    ttl = timeToLive;
-  }
+		// sensor id
+		this.id = id;
+		ttl = timeToLive;
+	}
 
-  /**
-   * method to keep running in the threads, only updates values.
-   */
-  @Override
-  public void run() {
-    updateValues();
-  }
+	/**
+	 * method to keep running in the threads, only updates values.
+	 */
+	@Override
+	public void run() {
+		updateValues();
+	}
 
-  /**
-   * creates a soap request for the sensor data, parses and saves it.
-   */
-  public void updateValues() {
-    String response = soap.sendRequest(Integer.toString(id));
-    if (response == null)
-      return;
+	/**
+	 * creates a soap request for the sensor data, parses and saves it.
+	 */
+	public void updateValues() {
+		String response = soap.sendRequest(Integer.toString(id));
+		if (response == null)
+			return;
 
-    Document doc = parseDOM(response);
-    if (doc == null)
-      return;
+		Document doc = parseDOM(response);
+		if (doc == null)
+			return;
 
-    NodeList sensors = Objects.requireNonNull(doc).getElementsByTagName("sensor");
+		NodeList sensors = Objects.requireNonNull(doc).getElementsByTagName("sensor");
 
-    for (int i = 0; i < sensors.getLength(); i++) {
-      Node sensor = sensors.item(i);
+		for (int i = 0; i < sensors.getLength(); i++) {
+			Node sensor = sensors.item(i);
 
-      NamedNodeMap atts = sensor.getAttributes();
-      String name = atts.getNamedItem("name").getTextContent();
-      double value = Double.parseDouble(sensor.getTextContent());
+			NamedNodeMap atts = sensor.getAttributes();
+			String name = atts.getNamedItem("name").getTextContent();
+			double value = Double.parseDouble(sensor.getTextContent());
 
-      if (!values.containsKey(name))
-        values.put(name, value);
-      values.replace(name, value);
-    }
+			if (!values.containsKey(name))
+				values.put(name, value);
+			values.replace(name, value);
+		}
 
-    lastUpdated = System.currentTimeMillis();
-  }
+		lastUpdated = System.currentTimeMillis();
+	}
 
-  /**
-   * parses the xml and saves it. (for testing purposes)
-   */
-  public void updateValues(String xml) {
-    Document doc = parseDOM(xml);
-    NodeList sensors = Objects.requireNonNull(doc).getElementsByTagName("sensor");
+	/**
+	 * parses the xml and saves it. (for testing purposes)
+	 */
+	public void updateValues(String xml) {
+		Document doc = parseDOM(xml);
+		NodeList sensors = Objects.requireNonNull(doc).getElementsByTagName("sensor");
 
-    for (int i = 0; i < sensors.getLength(); i++) {
-      Node sensor = sensors.item(i);
+		for (int i = 0; i < sensors.getLength(); i++) {
+			Node sensor = sensors.item(i);
 
-      NamedNodeMap atts = sensor.getAttributes();
-      String name = atts.getNamedItem("name").getTextContent();
-      double value = Double.parseDouble(sensor.getTextContent());
+			NamedNodeMap atts = sensor.getAttributes();
+			String name = atts.getNamedItem("name").getTextContent();
+			double value = Double.parseDouble(sensor.getTextContent());
 
-      if (!values.containsKey(name))
-        values.put(name, value);
-      values.replace(name, value);
-    }
+			if (!values.containsKey(name))
+				values.put(name, value);
+			values.replace(name, value);
+		}
 
-    lastUpdated = System.currentTimeMillis();
-  }
+		lastUpdated = System.currentTimeMillis();
+	}
 
-  /**
-   * Returns the requested value for this sensor, if the name doesn't exists throws an exception.
-   * If the time since the last update is greater than the ttl, throw exception.
-   *
-   * @param name the name of the parameter.
-   * @return the value requested
-   * @throws Exception if it doesnt exists, or it is too old.
-   */
-  public double getValue(String name) throws Exception {
-    // throw exception when out of date
-    if (System.currentTimeMillis() - lastUpdated > ttl)
-      throw new Exception("The values are too old, sensor" + id + " needs update.");
+	/**
+	 * Returns the requested value for this sensor, if the name doesn't exists
+	 * throws an exception. If the time since the last update is greater than the
+	 * ttl, throw exception.
+	 *
+	 * @param name
+	 *            the name of the parameter.
+	 * @return the value requested
+	 * @throws Exception
+	 *             if it doesnt exists, or it is too old.
+	 */
+	public double getValue(String name) throws Exception {
+		// throw exception when out of date
+		if (System.currentTimeMillis() - lastUpdated > ttl)
+			throw new Exception("The values are too old, sensor" + id + " needs update.");
 
-    if (values.containsKey(name))
-      return values.get(name);
+		if (values.containsKey(name))
+			return values.get(name);
 
-    // throws exception when value not found
-    throw new Exception("The value " + name + " was not found.");
-  }
+		// throws exception when value not found
+		throw new Exception("The value " + name + " was not found.");
+	}
 
-  /**
-   * creates the document builder that will be used later to parse the soap
-   * response.
-   */
-  private void createDOMBuilder() {
-    try {
-      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-      dBuilder = dbFactory.newDocumentBuilder();
+	/**
+	 * creates the document builder that will be used later to parse the soap
+	 * response.
+	 */
+	private void createDOMBuilder() {
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			dBuilder = dbFactory.newDocumentBuilder();
 
-    } catch (Exception e) {
-      logger.error("Error occurred while creating document builder.");
-      e.printStackTrace();
-      System.exit(1);
-    }
-  }
+		} catch (Exception e) {
+			logger.error("Error occurred while creating document builder.");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 
-  /**
-   * parses a soap request in xml format and returns the generated document.
-   *
-   * @param response the xml response of the soap weather service.
-   * @return a DOM document containing the xml tree.
-   */
-  private Document parseDOM(String response) {
-    try {
-      return dBuilder.parse(new InputSource(new StringReader(response)));
+	/**
+	 * parses a soap request in xml format and returns the generated document.
+	 *
+	 * @param response
+	 *            the xml response of the soap weather service.
+	 * @return a DOM document containing the xml tree.
+	 */
+	private Document parseDOM(String response) {
+		try {
+			return dBuilder.parse(new InputSource(new StringReader(response)));
 
-    } catch (Exception e) {
-      logger.warn("Error occurred while parsing the DOM.");
-    }
-    return null;
-  }
+		} catch (Exception e) {
+			logger.warn("Error occurred while parsing the DOM.");
+		}
+		return null;
+	}
 }
